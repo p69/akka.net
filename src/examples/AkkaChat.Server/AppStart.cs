edba617e14b7f3239765e88364e1151181299b29
pavel.shilyagov <shilyagov.p@gmail.com>
2016-03-22 01:24:20 +0300

    Moved projects. Added simple self-hosted SignalR chat.

---
+++ src/examples/AkkaChat.Server/AppStart.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace AkkaChat.Server
+{
+    public class AppStart
+    {
+        static void Main(string[] args)
+        {
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            string url = "http://localhost:8069";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/akka", map =>
+            {
+                // Setup the cors middleware to run before SignalR.
+                // By default this will allow all origins. You can 
+                // configure the set of origins and/or http verbs by
+                // providing a cors options with a different policy.
+                map.UseCors(CorsOptions.AllowAll);
+
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableDetailedErrors = true
+                };
+
+                // Run the SignalR pipeline. We're not using MapSignalR
+                // since this branch is already runs under the "/signalr"
+                // path.
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+}
---
+++ src/examples/AkkaChat.Server/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+
+namespace AkkaChat.Server
+{
+    public class ChatHub : Hub
+    {
+        private readonly HashSet<string> _users;
+
+        public ChatHub()
+        {
+            _users = new HashSet<string>();
+        }
+
+        public void Login(string userName)
+        {
+            if (string.IsNullOrWhiteSpace(userName) ||
+                _users.Contains(userName))
+            {
+                Clients.Caller.joinFailed();
+                return;
+            }
+            _users.Add(userName);
+            Clients.Others.userJoined(userName);
+            Clients.Caller.joinOk();
+        }
+
+        public void SendMessage(string userName, string text)
+        {
+            if (string.IsNullOrWhiteSpace(userName) || !_users.Contains(userName)) return;
+            Clients.Others.newMessage(userName, text);
+        }
+    }
+}
